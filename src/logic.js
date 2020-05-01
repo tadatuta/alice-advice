284910ce16ec0e9debb35b74b4a14f7a7f6fb295
Vladimir Grinenko <i@tadatuta.com>
2020-05-02 01:01:11 +0300

    refactor

--- src/logic.js
+++ src/logic.js
@@ -1,41 +1,36 @@
 const httpRequest = require('./lib/http-request');
 
-const URL = 'https://fucking-great-advice.ru/';
-const API_ENDPOINT = `${URL}api/`;
+const API_ENDPOINT = `https://fucking-great-advice.ru/api/v2/`;
 
 function getTTS(text) {
     return text
         .replace(/блять/, 'блляять')
         .replace(/ёпта/, 'ёппта')
+        .replace(/хуй/, 'хууйй')
 }
 
 module.exports = event => {
     const command = event.request.command.toLowerCase();
     if (command.includes('помощь') || command.includes('что ты умеешь')) {
-        return 'Я даю офигенные советы! Просто попроси еще совет или скажи на какую тему хочешь совет!';
+        return 'Я даю офигенные советы! Просто попроси еще совет или скажи на какую тему посоветовать!';
     }
 
     const category = event.request.nlu.intents.category;
 
-    if (category) {
-        return httpRequest(`${API_ENDPOINT}v2/random-advices-by-tag?tag=${category.slots.category.value}`)
+    return httpRequest(category ?
+        `${API_ENDPOINT}random-advices-by-tag?tag=${category.slots.category.value}` :
+        `${API_ENDPOINT}random-advices`
+    )
         .then(r => JSON.parse(r))
         .then(json => json.data[0].text)
-        .then(text => ({
-            text,
-            tts: getTTS(text)
-        }))
-    }
-
-    return httpRequest(`${API_ENDPOINT}random`)
-        .then(resp => JSON.parse(resp))
-        .then(resp => {
+        .then(text => {
             if (event.session.new) {
-                resp.text += ' Если хочешь еще один охуенный совет, скажи «ещё». Или скажи на какую тему хочешь совет.';
+                text += ' Если хочешь еще один охуенный совет, скажи «ещё». Или скажи на какую тему хочешь совет.';
             }
 
-            resp.tts = getTTS(resp.text);
-
-            return resp;
+            return {
+                text,
+                tts: getTTS(text)
+            };
         });
 }
