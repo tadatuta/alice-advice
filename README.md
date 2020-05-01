d9e24b0c8a74cb527e0eaebafed2e3c8c14680eb
Vladimir Grinenko <i@tadatuta.com>
2020-05-01 22:06:18 +0300

    Update

--- README.md
+++ README.md
@@ -3,3 +3,41 @@
 Навык fucking-great-advice.ru для Алисы.
 
 Описание API: https://fucking-great-advice.ru/api
+
+## Допустимые категории
+
+```js
+{
+    design: 'дизайн',
+    coding: 'код',
+    markup: 'вёрстка',
+    photo: 'фотография',
+    text: 'текст',
+    marketing: 'маркетинг',
+    seo: 'СЕО',
+    driving: 'вождение',
+    music: 'музыка',
+    tricks: 'фокусы',
+    medicine: 'медицина',
+    education: 'учёба',
+    life: 'за жизнь',
+    'for-him': 'для него',
+    'for-her': 'для неё',
+    newyear: 'новый год',
+    business: 'бизнес',
+    frontend: 'фронтэнд',
+    office: 'офис',
+    snowboard: 'сноуборд',
+    gtd: 'продуктивность',
+    love: 'отношения',
+    kids: 'воспитание',
+    money: 'деньги',
+    friendship: 'дружба',
+    translate: 'перевод',
+    dentist: 'стоматология',
+    travel: 'путешествия',
+    health: 'здоровье',
+    wwz: 'зомби апокалипсис',
+    zombie: 'зомби'
+}
+```
--- src/logic.js
+++ src/logic.js
@@ -1,19 +1,40 @@
 const httpRequest = require('./lib/http-request');
 
-const API_ENDPOINT = 'https://fucking-great-advice.ru/api/';
+const URL = 'https://fucking-great-advice.ru/';
+const API_ENDPOINT = `${URL}api/`;
+
+function getTTS(text) {
+    return text
+        .replace(/блять/, 'блляять')
+        .replace(/ёпта/, 'ёппта')
+}
 
 module.exports = event => {
     const command = event.request.command.toLowerCase();
     if (command.includes('помощь') || command.includes('что ты умеешь')) {
-        return 'Я даю офигенные советы! Просто попроси еще совет!';
+        return 'Я даю офигенные советы! Просто попроси еще совет или скажи на какую тему хочешь совет!';
+    }
+
+    const category = event.request.nlu.intents.category;
+
+    if (category) {
+        return httpRequest(`${API_ENDPOINT}v2/random-advices-by-tag?tag=${category.slots.category.value}`)
+        .then(r => JSON.parse(r))
+        .then(json => json.data[0].text)
+        .then(text => ({
+            text,
+            tts: getTTS(text)
+        }))
     }
 
     return httpRequest(`${API_ENDPOINT}random`)
         .then(resp => JSON.parse(resp))
         .then(resp => {
-            if (!event.session.new) return resp;
+            if (event.session.new) {
+                resp.text += ' Если хочешь еще один охуенный совет, скажи «ещё». Или скажи на какую тему хочешь совет.';
+            }
 
-            resp.text += ' Если хотите еще один охуенный совет, скажите «Ещё!»';
+            resp.tts = getTTS(resp.text);
 
             return resp;
         });
